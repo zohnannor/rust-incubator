d68ed21cffd6d87caab4dc9aeaa60aa8c5e661c0
zohnannor <zohnannor@gmail.com>
2022-06-28 15:08:42 +0300

    step 1.4 done

--- 1_concepts/1_4_cow/src/main.rs
+++ 1_concepts/1_4_cow/src/main.rs
@@ -1,3 +1,13 @@
+use std::{borrow::Cow, env};
+
 fn main() {
-    println!("Implement me!");
+    let path: Cow<'static, str> = if let Ok(path) = env::var("APP_CONF") {
+        path.into()
+    } else if let Some(path) = env::args().skip_while(|a| a != "--conf").nth(1) {
+        path.into()
+    } else {
+        "/etc/app/app.conf".into()
+    };
+
+    println!("{path}");
 }
