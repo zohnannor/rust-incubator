cbb61bf09204c7e97c9291d076210e536d10d99d
zohnannor <zohnannor@gmail.com>
2022-06-29 12:08:43 +0300

    step 1.5 done

--- 1_concepts/1_5_convert_cast_deref/src/main.rs
+++ 1_concepts/1_5_convert_cast_deref/src/main.rs
@@ -1,3 +1,14 @@
+use step_1_5::{email_string::EmailString, random::Random};
+
 fn main() {
-    println!("Implement me!");
+    let r = Random::from([
+        EmailString::new("1@hi.com").unwrap(),
+        EmailString::new("2@hi.com").unwrap(),
+        EmailString::new("3@hi.com").unwrap(),
+    ]);
+
+    for _ in 0..10 {
+        let email = &r;
+        println!("{email}");
+    }
 }
