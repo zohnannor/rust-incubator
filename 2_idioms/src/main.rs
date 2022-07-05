45fcbf036d721341dddb6011d08a0495df62a8ea
zohnannor <zohnannor@gmail.com>
2022-07-05 12:56:45 +0300

    step 2 done

--- 2_idioms/src/main.rs
+++ 2_idioms/src/main.rs
@@ -47,7 +47,7 @@ impl State for WaitingForCoin {}
 #[derive(Debug)]
 struct VendingMachine<S> {
     products: BTreeMap<Product, usize>,
-    money: Money,
+    coins: BTreeMap<Coin, usize>,
     state: S,
 }
 
@@ -55,7 +55,7 @@ impl Default for VendingMachine<Maintenance> {
     fn default() -> Self {
         Self {
             products: BTreeMap::default(),
-            money: Money::default(),
+            coins: BTreeMap::default(),
             state: Maintenance,
         }
     }
@@ -70,17 +70,22 @@ enum Error {
 
 impl VendingMachine<Maintenance> {
     fn with_money_amount(amount: usize) -> Self {
-        Self {
-            products: BTreeMap::new(),
-            money: Money::from_amount(amount),
-            state: Maintenance,
-        }
+        let money = Money::from_amount(amount);
+        Self::with_coins(money.0.into_iter())
     }
 
     fn with_coins(coins: impl IntoIterator<Item = Coin>) -> Self {
+        let mut coins_map = BTreeMap::new();
+        for coin in coins {
+            coins_map
+                .entry(coin)
+                .and_modify(|amount| *amount += 1)
+                .or_insert_with(|| 1);
+        }
+
         Self {
             products: BTreeMap::new(),
-            money: Money(coins.into_iter().collect()),
+            coins: coins_map,
             state: Maintenance,
         }
     }
@@ -100,10 +105,21 @@ impl VendingMachine<Maintenance> {
         self
     }
 
+    fn add_coins(&mut self, coins: impl IntoIterator<Item = Coin>) -> &mut Self {
+        for coin in coins {
+            self.coins
+                .entry(coin)
+                .and_modify(|amount| *amount += 1)
+                .or_insert(1);
+        }
+
+        self
+    }
+
     fn init(&mut self) -> VendingMachine<WaitingForUserChoice> {
         VendingMachine {
             products: mem::take(&mut self.products),
-            money: mem::take(&mut self.money),
+            coins: mem::take(&mut self.coins),
             state: WaitingForUserChoice,
         }
     }
@@ -111,18 +127,17 @@ impl VendingMachine<Maintenance> {
 
 impl VendingMachine<WaitingForUserChoice> {
     fn choose(self, name: ProductName) -> Result<VendingMachine<WaitingForCoin>, Error> {
-        dbg!(&self);
         match self.products.get_key_value(&name) {
             Some((_, amount)) if *amount == 0 => Err(Error::NotEnoughProducts(Self {
                 products: self.products,
-                money: self.money,
+                coins: self.coins,
                 state: WaitingForUserChoice,
             })),
             Some((product, _)) => {
                 let product = *product;
                 Ok(VendingMachine {
                     products: self.products,
-                    money: self.money,
+                    coins: self.coins,
                     state: WaitingForCoin {
                         product,
                         inserted: Money::default(),
@@ -142,25 +157,102 @@ impl VendingMachine<WaitingForCoin> {
     }
 
     fn try_get_product(
-        self,
+        mut self,
     ) -> Result<(VendingMachine<WaitingForUserChoice>, Product, Option<Money>), Error> {
-        let inserted = self
-            .state
-            .inserted
-            .0
-            .iter()
-            .map(|c| *c as u8 as usize)
-            .sum::<usize>();
+        let inserted = self.state.inserted.sum();
+        let price = self.state.product.price;
+        if inserted >= price {
+            let rest = inserted - price; // can't overflow, inserted >= price
 
-        let rest = self.state.product.price.checked_sub(inserted);
+            let rest = self.calc_rest(rest)?;
 
-        if rest.is_some() {}
-        todo!()
+            *self.products.get_mut(&self.state.product).unwrap() -= 1;
+
+            Ok((
+                VendingMachine {
+                    products: self.products,
+                    coins: self.coins,
+                    state: WaitingForUserChoice,
+                },
+                self.state.product,
+                rest,
+            ))
+        } else {
+            Err(Error::NotEnoughMoney(
+                VendingMachine {
+                    products: self.products,
+                    coins: self.coins,
+                    state: WaitingForUserChoice,
+                },
+                self.state.inserted,
+            ))
+        }
+    }
+
+    fn calc_rest(&mut self, rest: usize) -> Result<Option<Money>, Error> {
+        if rest == 0 {
+            Ok(None)
+        } else {
+            let rest = Money::from_amount(rest);
+
+            let machine_money = self
+                .coins
+                .iter()
+                .flat_map(|(&c, &amount)| std::iter::repeat(c as u8 as usize).take(amount))
+                .sum();
+
+            if rest.sum() > machine_money {
+                // trivial, rest is bigger than machine has
+                return Err(Error::CantGiveRest(
+                    VendingMachine {
+                        products: mem::take(&mut self.products),
+                        coins: mem::take(&mut self.coins),
+                        state: WaitingForUserChoice,
+                    },
+                    mem::take(&mut self.state.inserted), // return them their money back
+                ));
+            }
+
+            let mut machine_coins = self.coins.clone();
+
+            for coin in &self.state.inserted.0 {
+                machine_coins
+                    .entry(*coin)
+                    .and_modify(|amount| *amount += 1)
+                    .or_insert(1);
+            }
+
+            for coin in &rest.0 {
+                match machine_coins.get_mut(coin) {
+                    Some(amount) => *amount -= 1,
+                    None => {
+                        return Err(Error::CantGiveRest(
+                            VendingMachine {
+                                products: mem::take(&mut self.products),
+                                coins: mem::take(&mut self.coins),
+                                state: WaitingForUserChoice,
+                            },
+                            mem::take(&mut self.state.inserted), // return them their money back
+                        ));
+                    }
+                };
+            }
+
+            self.coins = machine_coins;
+
+            Ok(Some(rest))
+        }
     }
 }
 
 #[derive(Debug, Default)]
 struct Money(Vec<Coin>);
+
+impl PartialEq for Money {
+    fn eq(&self, other: &Self) -> bool {
+        self.sum() == other.sum()
+    }
+}
 
 impl Money {
     fn from_amount(mut amount: usize) -> Self {
@@ -189,9 +281,13 @@ impl Money {
 
         Self(coins)
     }
+
+    fn sum(&self) -> usize {
+        self.0.iter().map(|c| *c as u8 as usize).sum()
+    }
 }
 
-#[derive(Debug, Clone, Copy, num_enum::TryFromPrimitive)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, num_enum::TryFromPrimitive)]
 #[repr(u8)]
 enum Coin {
     One = 1,
@@ -203,7 +299,7 @@ enum Coin {
 }
 
 fn main() {
-    let vm = VendingMachine::with_money_amount(14)
+    let vm = VendingMachine::with_money_amount(17)
         .add_products(vec![
             Product::new(ProductName::ChocoBar, 23),
             Product::new(ProductName::Chips, 19),
@@ -211,12 +307,13 @@ fn main() {
             Product::new(ProductName::Soda, 15),
         ])
         .add_product(Product::new(ProductName::Water, 5))
+        .add_coins([Coin::One; 10])
         .init();
 
     let mut vm = vm.choose(ProductName::Chips).unwrap();
     vm.insert_coin(Coin::Ten);
 
-    let (vm, _money) = match vm.try_get_product() {
+    let (vm, _returned_money) = match vm.try_get_product() {
         Err(Error::NotEnoughMoney(vm, money)) => (vm, money),
         _ => unreachable!(),
     };
@@ -224,5 +321,13 @@ fn main() {
     let mut vm = vm.choose(ProductName::Chips).unwrap();
     vm.insert_coin(Coin::Ten);
     vm.insert_coin(Coin::Ten);
-    let (vm, product, rest) = vm.try_get_product().expect("inserted required amount");
+    let (_vm, product, rest) = vm.try_get_product().unwrap();
+    assert_eq!(
+        product,
+        Product {
+            name: ProductName::Chips,
+            price: 19
+        }
+    );
+    assert_eq!(rest, Some(Money::from_amount(1)));
 }
