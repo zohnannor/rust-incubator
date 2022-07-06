abf4a480abd735d1902f85a73459418ce20d6b7e
zohnannor <zohnannor@gmail.com>
2022-07-06 14:07:49 +0300

    step 3.2 done

--- 3_ecosystem/3_2_macro/src/main.rs
+++ 3_ecosystem/3_2_macro/src/main.rs
@@ -1,3 +1,52 @@
+mod declarative;
+
 fn main() {
-    println!("Implement me!");
+    use std::collections::BTreeMap;
+    {
+        let _map = declarative::btreemap! {
+            1 => "hello",
+            2 => "world!"
+        };
+
+        let x = "asd";
+        let _map = declarative::btreemap! {
+            x => "trailing",
+            x => "comma ->",
+        };
+
+        let _map: BTreeMap<String, Vec<usize>> = declarative::btreemap! {};
+
+        let _map = declarative::btreemap! {
+            1 => declarative::btreemap! {
+                1 => "nested",
+                2 => "nested"
+            }
+        };
+    }
+
+    {
+        let _map = procedural::btreemap! {
+            1 => "hello",
+            2 => "world!"
+        };
+
+        let x = "asd";
+        let _map = procedural::btreemap! {
+            x => "trailing",
+            x => "comma ->",
+        };
+
+        let _map: BTreeMap<String, Vec<usize>> = procedural::btreemap! {};
+
+        // error: expected `,`
+        // 1 => procedural::btreemap! {
+        //                          ^
+
+        // let _map = procedural::btreemap! {
+        //     1 => procedural::btreemap! {
+        //         1 => "nested",
+        //         2 => "nested"
+        //     }
+        // };
+    }
 }
