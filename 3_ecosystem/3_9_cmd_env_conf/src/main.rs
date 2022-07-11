d27e40a957d0992b27b6b6b816296c381a7ada88
zohnannor <zohnannor@gmail.com>
2022-07-11 15:00:18 +0300

    progress on step 3.9

--- 3_ecosystem/3_9_cmd_env_conf/src/main.rs
+++ 3_ecosystem/3_9_cmd_env_conf/src/main.rs
@@ -1,3 +1,28 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+mod settings;
+
+#[derive(clap::Parser)]
+#[clap(author, version, long_about = None)]
+struct Args {
+    /// Enables debug mode
+    #[clap(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    /// Path to configuration file
+    #[clap(short, long, env = "CONF_FILE", default_value = "config.toml")]
+    conf: PathBuf,
+}
+
 fn main() {
-    println!("Implement me!");
+    let Args { debug, conf } = Args::parse();
+
+    let config = config::Config::builder()
+        .add_source(config::File::with_name("config.toml"))
+        .build()
+        .unwrap();
+
+    dbg!(debug, conf);
 }
